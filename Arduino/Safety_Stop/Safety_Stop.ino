13f723c45823af1b40817f96642f71eaecbc0715
Lucas Maurice <lucas.maurice@outlook.com>
2017-07-03 20:11:05 -0400

    Clean and improve Hardware code

--- Arduino/Safety_Stop/Safety_Stop.ino
+++ Arduino/Safety_Stop/Safety_Stop.ino
@@ -1,194 +1,286 @@
 /*
  * ATmega328P based safety stop
- * Service: safety_stop_srv
+ * Service: _safety_stop_srv
  * Topic: start_button
  */
 
 #include <ros.h>
-#include <std_msgs/String.h>
 #include <std_msgs/Bool.h>
 #include <std_srvs/SetBool.h>
 
+
+#define START_BUTTON    2       //PIN for start button
+#define SAFETY_BUTTON   4       //PIN for safety button
+#define POWER_CTRL      3       //PIN for power relay
+#define LED_PIN         13      //PIN for LED feedback
+
+#define SHORT_INTERVAL 100      //LED short blink interval
+#define REGULAR_INTERVAL 500    //LED regular blink interval
+#define LONG_INTERVAL 1000      //LED long blink interval
+
+#define DEBOUNCE_DELAY 500      //delay of bounce for debouncers
+
+#define TIMEOUT_FEEDBACK 1000   //timeout for service caller feedback
+
+#define PUBLISH_INTERVAL 200
+
+
 typedef enum{
-  RUNNING =1,
-  SAFETY_STOP,
-  REQUEST_PENDING,
-  START_PENDING,
-}State;
+	RUN,
+	STOP,
+	LAPTOP_FEEDBACK_PENDING,
+	START_PENDING,
+	START_BACK,
+	WAIT_START_RAISED,
+	SHUTDOWN
+} T_State;
 
-const float V_MAX = 26;
-const float V_MIN = 22;
 
-const int START_BUTTON = 2;
-const int SAFETY_BUTTON = 4;
-const int POWER_CTRL = 3;
-const int LED_PIN = 13;
+//System state
+T_State system_state;
 
-const int REGULAR_INTERVAL = 500;
-const int LONG_INTERVAL = 1000;
-const int NUMBER_OF_TRY = 3;
-const int DEBOUNCE_DELAY = 500;
-
-bool start_last_reading = false;
-bool stop_last_reading;
-bool stop_enter = true;
-bool start_enter = true;
-unsigned long stop_last_debounce_time = 0;
-unsigned long start_last_debounce_time = 0;
-unsigned long led_last_time = 0;
-
-int power_state = HIGH;
-State system_state;
-
+//create ros handle
 ros::NodeHandle  nh;
 
-ros::ServiceClient<std_srvs::SetBool::Request, std_srvs::SetBool::Response> safety_stop_srv("safety_stop_srv");
-
+//generate start message
 std_msgs::Bool start_msg;
+
+//generate start button publisher
 ros::Publisher start_button("start_button_msg", &start_msg);
 
-void start_button_handle();
-void safety_stop_handler();
-void led_handler(const int time);
+//initialise service client
+ros::ServiceClient<std_srvs::SetBool::Request, std_srvs::SetBool::Response> _safety_stop_srv("safety_stop_srv");
 
-void setup()
-{
-  pinMode(START_BUTTON, INPUT);
-  pinMode(SAFETY_BUTTON, INPUT);
-  pinMode(POWER_CTRL, OUTPUT);
-  pinMode(LED_PIN, OUTPUT);
-  
-  digitalWrite(POWER_CTRL, power_state);
+bool start_button_handle();
 
-  stop_last_reading = digitalRead(SAFETY_BUTTON);
-  if(stop_last_reading == HIGH)
-  {
-    system_state = RUNNING;
-  }
-  else
-  {
-    system_state = SAFETY_STOP;
-  }
-  
-  nh.initNode();
-  nh.advertise(start_button);
-  nh.serviceClient(safety_stop_srv);
-  
-  while(!nh.connected()) nh.spinOnce();
-  nh.loginfo("Safety stop startup complete");
+bool stop_button_handle();
+
+bool stop_init;
+
+void led_handler();
+
+void setup() {
+	//set pinout
+	pinMode(START_BUTTON, INPUT);
+	pinMode(SAFETY_BUTTON, INPUT);
+	pinMode(POWER_CTRL, OUTPUT);
+	pinMode(LED_PIN, OUTPUT);
+
+	//set on power relay
+	digitalWrite(POWER_CTRL, HIGH);
+
+	//read initial state of STOP switch
+	stop_init = digitalRead(SAFETY_BUTTON);
+
+	if (stop_init == HIGH) {
+		system_state = RUN;
+	} else {
+		system_state = START_PENDING;
+	}
+
+	//ros initialisations
+	nh.initNode();
+	nh.advertise(start_button);
+	nh.serviceClient(_safety_stop_srv);
+
+	while (!nh.connected()) nh.spinOnce();
 }
 
-void loop()
-{
-  
-  if(system_state == RUNNING)
-  { 
-    start_button_handle();
-    safety_stop_handler();
-    led_handler(REGULAR_INTERVAL);
-  }
-  
-  if(system_state == SAFETY_STOP)
-  {
-    digitalWrite(LED_PIN, HIGH);
-    safety_stop_handler();
-  }
-  
-  if(system_state == START_PENDING)
-  {
-    start_button_handle();
-    safety_stop_handler();
-    led_handler(LONG_INTERVAL);
-  }
-  
-  digitalWrite(POWER_CTRL, power_state);
-  nh.spinOnce();
+void loop() {
+	static unsigned long firstServiceCallTime = 0;
+	static unsigned long publisherTime = 0;
+	static bool bFeedbackReceived = false;
+	static bool bTimeout = false;
+
+	//------ input handler ------
+
+	bool bStopPressed = !stop_button_handle();
+	bool bStartPressed = start_button_handle();
+
+	//------ StateMachine ------
+
+	switch (system_state) {
+		case RUN:                                   //System is running normally, waiting for press on STOP button.
+			if (bStopPressed) {
+				system_state = STOP;
+			}
+			if (bStartPressed) {
+				system_state = START_BACK;
+			}
+			break;
+
+		case STOP:                                  //System go in stop mode, call a ROS service for inform main system.
+			system_state = LAPTOP_FEEDBACK_PENDING;
+			break;
+
+		case LAPTOP_FEEDBACK_PENDING:               //System wait for main system answer.
+			if (bFeedbackReceived) {
+				system_state = START_PENDING;           //IF answer, will wait for press on START button.
+			} else if (bTimeout) {
+				system_state = SHUTDOWN;                //IF timeout, go to shutdown.
+			}
+			break;
+
+		case START_PENDING:                         //System is waiting for press on START button for go back in normal running mode.
+			if (bStartPressed && !bStopPressed) {
+				system_state = START_BACK;
+			}
+			break;
+
+		case START_BACK:
+			if (bStopPressed) {
+				system_state = STOP;
+			} else {
+				system_state = WAIT_START_RAISED;
+			}
+			break;
+
+		case WAIT_START_RAISED:
+			if (bStopPressed) {
+				system_state = STOP;
+			} else if (!bStartPressed) {
+				system_state = RUN;
+			}
+			break;
+
+		case SHUTDOWN:                              //System is going to shutdown.
+			break;
+	}
+
+	//------ output handlers ------
+
+	//LED Handle
+	led_handler();
+
+	//Power Relay Control
+	digitalWrite(POWER_CTRL, system_state != SHUTDOWN);
+
+	//ros initialise service caller
+	if (system_state == START_BACK) {
+		std_srvs::SetBool::Request req;
+		std_srvs::SetBool::Response res;
+
+		//write message
+		req.data = false;
+
+		//call RESTART service
+		_safety_stop_srv.call(req, res);
+	}
+
+	//ros initialise service caller
+	if (system_state == STOP) {
+		//initialise timeout
+		firstServiceCallTime = millis();
+		bFeedbackReceived = false;
+		bTimeout = false;
+	}
+
+	//ros service caller
+	if (system_state == STOP || system_state == LAPTOP_FEEDBACK_PENDING) {
+		if ((millis() - firstServiceCallTime) > TIMEOUT_FEEDBACK) {
+			bTimeout = true;
+		} else {
+			//create message
+			std_srvs::SetBool::Request req;
+			std_srvs::SetBool::Response res;
+
+			//write message
+			req.data = true;
+
+			//call STOP service
+			_safety_stop_srv.call(req, res);
+
+			delay(10);
+
+			//if success, raise the flag
+			if (res.success) {
+				bFeedbackReceived = true;
+			}
+		}
+	}
+
+	//ros publisher
+	if ((millis() - publisherTime) > PUBLISH_INTERVAL && system_state != SHUTDOWN) {
+		publisherTime = millis();
+		start_msg.data = system_state == RUN || system_state == START_BACK || system_state == WAIT_START_RAISED;
+		start_button.publish(&start_msg);
+	}
+
+	nh.spinOnce();
 }
 
-void start_button_handle()
-{
-  bool reading = digitalRead(START_BUTTON);
-  
-  if((millis() - start_last_debounce_time) > DEBOUNCE_DELAY)
-  {
-    start_enter = true;
-  }
-  
-  if (start_last_reading != reading && start_enter){
-    
-    start_last_debounce_time = millis();
-    start_enter = false;
-    
-    start_last_reading = reading;
-    if(reading){
-      start_msg.data = reading;
-      start_button.publish(&start_msg);
-      system_state = RUNNING;
-      power_state = HIGH;
-    }
-  }
+bool start_button_handle() {
+	static bool start_last_reading = false;
+	static bool start_enter = true;
+	static unsigned long start_last_debounce_time = 0;
+
+	//read start button state
+	bool reading = digitalRead(START_BUTTON);
+
+	//check if bounce
+	if ((millis() - start_last_debounce_time) > DEBOUNCE_DELAY) {
+		start_enter = true;
+	}
+
+	if (reading != start_last_reading && start_enter) {
+		start_last_debounce_time = millis();
+		start_enter = false;
+
+		//update start button state
+		start_last_reading = reading;
+	}
+	return start_last_reading;
 }
 
-void safety_stop_handler()
-{
-  bool reading = digitalRead(SAFETY_BUTTON);
-  
-  if((millis() - stop_last_debounce_time) > DEBOUNCE_DELAY)
-  {
-    stop_enter = true;
-  }
-  
-  if (reading != stop_last_reading && stop_enter)
-  {
-    stop_last_debounce_time = millis();
-    stop_enter = false;
-    
-    std_srvs::SetBool::Request req;
-    std_srvs::SetBool::Response res;
-    stop_last_reading = reading;
-    req.data = reading;
-    
-    if(reading == HIGH)
-    {
-      system_state = START_PENDING;
-      safety_stop_srv.call(req, res);
-    }
-    else
-    {
-      system_state = REQUEST_PENDING;
-      for(int i=0; i<NUMBER_OF_TRY; i++)
-      {
-        safety_stop_srv.call(req, res);
-        if(res.success)
-        {
-          system_state = SAFETY_STOP;
-          power_state = HIGH;
-          break;
-        }
-        delay(10);
-      }
-    }
-    
-    if(system_state == REQUEST_PENDING)
-    {
-      system_state = SAFETY_STOP;
-      power_state = LOW;
-    }
-  }
+bool stop_button_handle() {
+	static bool stop_last_reading = stop_init;
+	static bool stop_enter = true;
+	static unsigned long stop_last_debounce_time = 0;
+
+	//read stop button
+	bool reading = digitalRead(SAFETY_BUTTON);
+
+	//check if bounce
+	if ((millis() - stop_last_debounce_time) > DEBOUNCE_DELAY) {
+		stop_enter = true;
+	}
+
+	if (reading != stop_last_reading && stop_enter) {
+		stop_last_debounce_time = millis();
+		stop_enter = false;
+
+		//update start button state
+		stop_last_reading = reading;
+	}
+	return stop_last_reading;
 }
 
-void led_handler(const int time)
-{
-  
-  if (millis() - led_last_time >= time)
-  {
-    led_last_time = millis();
-    digitalWrite(LED_PIN, HIGH - digitalRead(LED_PIN));
-  }
-}
+void led_handler() {
+	static unsigned long led_last_time = 0;
+	int time;
+	switch (system_state) {
+		case RUN:
+			time = LONG_INTERVAL;
+			break;
 
-void voltage_check()
-{
-  
-}
+		case STOP:
+		case LAPTOP_FEEDBACK_PENDING:
+			time = REGULAR_INTERVAL;
+			break;
 
+		case START_PENDING:
+			time = SHORT_INTERVAL;
+			break;
+
+		default:
+			time = 0;
+	}
+
+	if (time == 0) {
+		led_last_time = millis();
+		digitalWrite(LED_PIN, HIGH);
+	} else if (millis() - led_last_time >= time) {
+		led_last_time = millis();
+		digitalWrite(LED_PIN, !digitalRead(LED_PIN));
+	}
+}
